6eb8b76d1435c96dff29d22fe1c9f62f999f1c44
shuhei <kawashu0723@gmail.com>
2016-11-26 15:05:48 +0900

    setup

---
+++ LNFloatingActionButton-Example/LNFloatingActionButton-Example/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  LNFloatingActionButton-Example
+//
+//  Created by Shuhei Kawaguchi on 2016/11/26.
+//  Copyright © 2016年 Shuhei Kawaguchi. All rights reserved.
+//
+
+import UIKit
+import LNFloatingActionButton
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ LNFloatingActionButton/LNFloatingActionButton.h
@@ -0,0 +1,19 @@
+//
+//  LNFloatingActionButton.h
+//  LNFloatingActionButton
+//
+//  Created by Shuhei Kawaguchi on 2016/11/26.
+//  Copyright © 2016年 Shuhei Kawaguchi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LNFloatingActionButton.
+FOUNDATION_EXPORT double LNFloatingActionButtonVersionNumber;
+
+//! Project version string for LNFloatingActionButton.
+FOUNDATION_EXPORT const unsigned char LNFloatingActionButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LNFloatingActionButton/PublicHeader.h>
+
+
