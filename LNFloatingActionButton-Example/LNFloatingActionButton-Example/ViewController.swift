45b7ee3b23654628af57771eae8f506d2db9520b
shuhei <kawashu0723@gmail.com>
2017-10-01 22:43:59 +0900

    change top view to tableview

--- LNFloatingActionButton-Example/LNFloatingActionButton-Example/ViewController.swift
+++ LNFloatingActionButton-Example/LNFloatingActionButton-Example/ViewController.swift
@@ -10,48 +10,14 @@ import UIKit
 import LNFloatingActionButton
 
 class ViewController: UIViewController {
+    @IBOutlet weak var sampleTableView: UITableView!
     
-    var cells: [LNFloatingActionButtonCell] = []
-    var floatingActionButton: LNFloatingActionButton!
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let ellipseCell: LNFloatingActionButtonEllipseCell = {
-            let cell = LNFloatingActionButtonEllipseCell()
-            cell.ellipseSize = CGSize(width: 150, height: 30)
-            cell.titleTextAlignment = .center
-            cell.title = "like button"
-            cell.image = UIImage(named: "like")
-            return cell
-        }()
-        cells.append(ellipseCell)
-        
-        let ellipse2Cell: LNFloatingActionButtonEllipseCell = {
-           let cell = LNFloatingActionButtonEllipseCell()
-            cell.ellipseSize = CGSize(width: 150, height: 30)
-            cell.titleTextAlignment = .center
-            cell.title = "home button"
-            cell.image = UIImage(named: "home")
-            return cell
-        }()
-        cells.append(ellipse2Cell)
-        
-        let fab: LNFloatingActionButton = {
-            let button = LNFloatingActionButton(x: view.frame.size.width - 100, y: view.frame.size.height - 100)
-            button.delegate = self
-            button.dataSource = self
-            button.color = .white
-            button.shadowOffset = CGSize(width: 0.0, height: 2.0)
-            button.shadowOpacity = 0.5
-            button.shadowRadius = 2.0
-            button.shadowPath = button.circlePath
-            button.closedImage = UIImage(named: "plus")
-            button.cellHorizontalAlign = .right
-            button.isBackgroundView = true
-            return button
-        }()
-        view.addSubview(fab)
+        self.title = "sample"
+        sampleTableView.delegate = self
+        sampleTableView.dataSource = self
+        sampleTableView.tableFooterView = UIView()
     }
 
     override func didReceiveMemoryWarning() {
@@ -59,28 +25,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
 }
 
-
-extension ViewController: LNFloatingActionButtonDelegate {
-    func floatingActionButton(_ floatingActionButton: LNFloatingActionButton, didSelectItemAtIndex index: Int) {
-        print(index)
-    }
-}
-
-extension ViewController: LNFloatingActionButtonDataSource {
-    func numberOfCells(_ floatingActionButton: LNFloatingActionButton) -> Int {
-        return cells.count
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
     }
     
-    func cellForIndex(_ index: Int) -> LNFloatingActionButtonCell {
-        return cells[index]
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        if indexPath.row == 0 {
+            cell.textLabel?.text = "normal cell"
+        }
+        return cell
     }
 }
 
-extension LNFloatingActionButton {
-    func customeAnimation() {
-        // original animation ...
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch indexPath.row {
+        case 0:
+            let vc = UIStoryboard(name: "Main", bundle: nil)
+                .instantiateViewController(withIdentifier: "EllipseCellViewController") as! EllipseCellViewController
+            self.navigationController?.pushViewController(vc, animated: true)
+            
+        default:
+            return
+        }
     }
 }
